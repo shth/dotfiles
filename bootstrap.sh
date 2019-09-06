012447c92ac73d37813deda54cf8bd53532c0d31
shth <shthftwyes@gmail.com>
2019-09-06 15:29:43 +0800

    misc: added .config folder

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,9 +9,12 @@ mv -ivn ~/.gitconfig ~/.gitconfig.default
 mv -ivn ~/.tmux.conf ~/.tmux.conf.default
 mv -ivn ~/.vimrc ~/.vimrc.default
 mv -ivn ~/.zshrc ~/.zshrc.default 
+rsync -av ~/.config/ ~/.config.default/ 
 
 echo "Adding symlinks..." 
 ln -sfnv $PWD/.gitconfig ~/.gitconfig
 ln -sfnv $PWD/.tmux.conf ~/.tmux.conf
 ln -sfnv $PWD/.vimrc ~/.vimrc
 ln -sfnv $PWD/.zshrc ~/.zshrc
+rm -rf ~/.config
+ln -sfnv $PWD/.config ~/.config
--- cleanup.sh
+++ cleanup.sh
@@ -3,3 +3,6 @@ mv -iv ~/.gitconfig.default ~/.gitconfig
 mv -iv ~/.tmux.conf.default ~/.tmux.conf
 mv -iv ~/.vimrc.default ~/.vimrc
 mv -iv ~/.zshrc.default ~/.zshrc
+rm -rf ~/.config
+rsync -av ~/.config.default/ ~/.config/
+rm -rf ~/.config.default
