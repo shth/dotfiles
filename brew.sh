8768c023e05e529d0021a429bed4c648636b0cdd
Stephen Hung <stephenhung@hk01.com>
2019-10-11 09:36:38 +0800

    fix brew script install typo

--- brew.sh
+++ brew.sh
@@ -3,19 +3,19 @@
 
 brew update && brew upgrade
 
-brew instal awscli
-brew instal fzf
-brew instal git
-brew instal httpie
-brew instal hub
-brew instal mongodb
-brew instal node
-brew instal openssl
-brew instal python
-brew instal python@2
-brew instal ruby
-brew instal the_silver_searcher
-brew instal tmux
-brew instal vim
-brew instal yarn
-brew instal zsh
+brew install awscli
+brew install fzf
+brew install git
+brew install httpie
+brew install hub
+brew install mongodb
+brew install node
+brew install openssl
+brew install python
+brew install python@2
+brew install ruby
+brew install the_silver_searcher
+brew install tmux
+brew install vim
+brew install yarn
+brew install zsh
