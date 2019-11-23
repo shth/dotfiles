ff5fc2a9ccde9c4988b16b52812882669007ab07
Stephen Hung <stephenhung@hk01.com>
2019-11-23 20:33:25 +0800

    fixup! misc: fixed bootstrap script

--- mac/install.sh
+++ mac/install.sh
@@ -1,28 +1,32 @@
 # use this mapping to google what each package does
 # map <leader>g :!chrome-cli open "https://www.google.com/search?q=<C-R><C-W> github"<CR>
 
-sudo apt update && sudo apt upgrade -y
+brew update && brew upgrade
 
-sudo apt install fzf
-sudo apt install git
-sudo apt install httpie
-# installing nodejs
-sudo apt install curl
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs
-
-sudo apt install ruby
-
-# this requires Ubuntu v18.10.0 or above
-sudo apt-get install ripgrep
-
-sudo apt install tmux
-
-# install vim-gtk for clipboard support
-sudo apt install vim-gtk
-
-sudo apt install zsh
+brew install awscli
+brew install fzf
+brew install git
+brew install httpie
+brew install hub
+brew install mongodb
+brew install node
+brew install openssl
+brew install python
+brew install python@2
+brew install ruby
+brew install the_silver_searcher
+brew install tmux
+brew install vim
+brew install yarn
+brew install zsh
 
 # oh-my-zsh
 echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "Installing TPM(tmux package manager)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Installing dircolors"
+git clone https://github.com/seebi/dircolors-solarized.git ./libraries/dircolors-solarized
+
