c76006a54e7e8b17987d148398763baa509aaf94
Stephen Hung <stephenhung@hk01.com>
2020-06-01 11:19:12 +0800

    WSL: added WSLGitWrappers for better git performance

---
+++ WSLGitWrapper.sh
@@ -0,0 +1,15 @@
+#!/bin/bash                                                             
+# WSL 'git' wrapper, save as /usr/local/bin/git and chmod a+x           
+                                                                        
+REALPATH=`readlink -f ${PWD}`                                           
+ARG1=$1                                                                 
+                                                                        
+if [  "${REALPATH:0:5}" == "/mnt/" ]; then                              
+  if [ $ARG1 == "log" ]; then                                    
+    git.exe "$@" | more                                                 
+  else                                                                  
+    git.exe "$@"                                                        
+  fi                                                                    
+else                                                                    
+  /usr/bin/git "$@"                                                     
+fi                 
